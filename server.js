e72a742eebc5d6f991857fbd38c3993f916eb692
Richie Casto <rcasto92@gmail.com>
2019-05-25 12:40:37 -0700

    starting templating with ejs

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send("Hello World!");
+    res.render('index');
 });
 
 app.listen(port,
