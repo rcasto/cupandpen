50afc51da516b65542981a47a0faf18c83cade3d
Richie Casto <rcasto92@gmail.com>
2019-05-26 15:31:41 -0700

    snapshot

---
+++ contentStorageService.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const util = require('util');
+const md = require('markdown-it')();
+const readFile = util.promisify(fs.readFile);
+const readdir = util.promisify(fs.readdir);
+
+const markdownFileRegex = /^.*\.md$/;
+const dataEncoding = 'utf8';
+const contentStore = { };
+
+async function init(contentPath) {
+    try {
+        var contentFiles = (await Promise.all((await readdir(contentPath, {
+                encoding: dataEncoding,
+                withFileTypes: true,
+            }))
+            .filter(contentFile => contentFile && contentFile.isFile())
+            .filter(contentFile => markdownFileRegex.test(contentFile.name))
+            .map(async contentFile => ({
+                name: contentFile.name,
+                data: await readFile(`./${contentPath}/${contentFile.name}`, dataEncoding)
+            }))
+        ));
+
+        contentFiles
+            .forEach(contentFile => contentStore[contentFile.name] = contentFile);
+    } catch (err) {
+        console.error(`Failed to load content: ${err}`);
+    }
+}
+
+function getAllContent() {
+    return Object.values(contentStore);
+}
+
+function getContent(name) {
+    if (contentStore[name]) {
+        return contentStore[name];
+    }
+    return null;
+}
+
+module.exports = async function (contentPath) {
+    await init(contentPath);
+    return {
+        getAllContent,
+        getContent,
+    };
+};
--- server.js
+++ server.js
@@ -1,51 +1,24 @@
-const fs = require('fs');
-const util = require('util');
 const express = require('express');
+const contentStorageServiceFactory = require('./contentStorageService');
 
-const readFile = util.promisify(fs.readFile);
-const readdir = util.promisify(fs.readdir);
-
-const dataDirectory = './data';
-const dataEncoding = 'utf8';
-const jsonFileRegex = /^.*\.json$/;
+const contentDirectory = './content';
 const port = 3000;
-const app = express();
 
-// Load content into memory
-loadContent();
+async function init() {
+    const contentStorageService = await contentStorageServiceFactory(contentDirectory);
+    const app = express();
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+    app.set('views', './views');
+    app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+    app.get('/', (req, res) => {
+        res.render('index', {
+            contentList: contentStorageService.getAllContent(),
+        });
+    });
 
-app.listen(port,
-    () => console.log(`Server started on port ${port}`));
-
-async function loadContent() {
-    try {
-        var contentConfigs = (await Promise.all((await readdir(dataDirectory, {
-                encoding: dataEncoding,
-                withFileTypes: true,
-            }))
-            .filter(contentConfig => contentConfig && contentConfig.isFile())
-            .filter(contentConfig => jsonFileRegex.test(contentConfig.name))
-            .map(async contentConfig => await readFile(`./${dataDirectory}/${contentConfig.name}`, dataEncoding))
-        ))
-        .map(contentConfig => {
-            try {
-                return JSON.parse(contentConfig);
-            } catch(err) {
-                return null;
-            }
-        })
-        .filter(contentConfig => !!contentConfig);
-
-        contentConfigs
-            .forEach(contentConfig => console.log(contentConfig.title));
-    } catch (err) {
-        console.error(`Failed to load content: ${err}`);
-    }
+    app.listen(port,
+        () => console.log(`Server started on port ${port}`));
 }
+
+init();
