6ad55ac74cb0814be867a5449a7f6839cf94424e
Richie Casto <rcasto92@gmail.com>
2019-05-27 13:22:58 -0700

    fix port setup

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const contentStorageServiceFactory = require('./contentStorageService');
 
 const contentDirectory = './content';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 function init() {
     const contentStorageServicePromise = contentStorageServiceFactory(contentDirectory);
