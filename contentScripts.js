567f958a12009da9ab9e991729516c1992a45222
Richie Casto <rcasto92@gmail.com>
2019-06-16 15:56:06 -0700

    add shelljs, update authoring client sync script

--- contentScripts.js
+++ contentScripts.js
@@ -3,14 +3,17 @@ const {
     SharedKeyCredential,
     ContainerSASPermissions
 } = require('@azure/storage-blob');
+const shell = require('shelljs');
 
 const account = "cupandpen";
 const accountKey = "MFgRtBFNvfMl8tYN089zBbfIxMbg7AppTCmGx8GxEpG8T/3JI3WqTaMjPv3gA37sPPP1KNTu0l6OlJKwh4Zj+g==";
 const containerName = `content`;
+const localFolder = 'content';
+const expirationTimeInMs = 300000; // 5 minutes
 
 const sharedKeyCredential = new SharedKeyCredential(account, accountKey);
 
-var containerSasPermissions = new ContainerSASPermissions();
+const containerSasPermissions = new ContainerSASPermissions();
 containerSasPermissions.add = true;
 containerSasPermissions.create = true;
 containerSasPermissions.write = true;
@@ -21,7 +24,15 @@ containerSasPermissions.delete = true;
 const sasQueryParameters = generateBlobSASQueryParameters({
     containerName,
     permissions: containerSasPermissions.toString(),
-    expiryTime: new Date(Date.now() + 1800000)
+    expiryTime: new Date(Date.now() + expirationTimeInMs)
 }, sharedKeyCredential);
 
-console.log(sasQueryParameters.toString());
+const syncCommand = `azcopy sync "${localFolder}" "https://${account}.blob.core.windows.net/${containerName}?${sasQueryParameters.toString()}" --recursive --delete-destination=true`;
+shell.exec(syncCommand, (code, stdout, stderr) => {
+    console.log('Exit code:', code);
+    if (code !== 0) {
+        console.log(`Error: ${stderr}`);
+    } else {
+        console.log(stdout);
+    }
+});
