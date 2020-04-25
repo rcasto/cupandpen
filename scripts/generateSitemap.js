eedb9ab7f949e439e92e9e646d7feaac1b7e0bce
Richie Casto <rcasto92@gmail.com>
2020-04-24 21:23:29 -0700

    new sitemap generator script

---
+++ scripts/generateSitemap.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const util = require('util');
+const { SitemapStream, streamToPromise } = require('sitemap');
+const fileStorageService = require('../lib/fileStorageService');
+
+const fsWriteFilePromise = util.promisify(fs.writeFile);
+
+const siteUrl = 'https://cupandpen.com';
+const sitemapPath = 'public/sitemap.xml';
+
+async function generateSitemap() {
+    try {
+        const allContentMetadata = await fileStorageService.getAllContent();
+        const sitemap = new SitemapStream({
+            hostname: siteUrl
+        });
+        const currentDate = new Date();
+
+        allContentMetadata
+            .forEach(contentMetadata => {
+                sitemap.write({
+                    url: `${siteUrl}/content/${contentMetadata.name}`,
+                    lastmod: currentDate,
+                });
+            });
+        sitemap.end();
+
+        const sitemapBuffer = await streamToPromise(sitemap);
+        const sitemapString = sitemapBuffer.toString();
+
+        await fsWriteFilePromise(sitemapPath, sitemapString);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+generateSitemap();
