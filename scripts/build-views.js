0ff990fd754e6fa4499196bf7a1fa0208b4a8061
Richie Casto <rcasto92@gmail.com>
2020-12-15 21:56:49 -0500

    update script to build static content views, integrate as npm script

--- scripts/build-views.js
+++ scripts/build-views.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const ejs = require('ejs');
+const fs = require('fs');
+const util = require('util');
 
 const contentIndex = require('../index.json');
 const indexView = path.resolve('views/index.ejs');
 const contentView = path.resolve('views/content.ejs');
+
+const writeFilePromise = util.promisify(fs.writeFile);
+
+const indexViewOutput = path.resolve('public/index.html');
+const contentViewOutput = path.resolve('public/content');
 
 async function renderView(viewPath, data) {
     return await ejs.renderFile(viewPath, data);
@@ -26,9 +33,21 @@ async function renderContentIndexView() {
 }
 
 async function generateViews() {
-    const contentIndexView = await renderContentIndexView();
+    try {
+        const contentIndexView = await renderContentIndexView();
+        await writeFilePromise(indexViewOutput, contentIndexView);
 
-    console.log(contentIndexView);
+        Object.keys(contentIndex)
+            .map(async contentName => {
+                const contentViewPath = path.resolve(contentViewOutput, `${contentName}.html`);
+                const contentNameView = await renderContentView(contentName);
+                await writeFilePromise(contentViewPath, contentNameView);
+            });
+
+        console.log('Content views successfully built!')
+    } catch (err) {
+        console.error(`An error occurred building content views\n${err}`);
+    }
 }
 
 generateViews()
