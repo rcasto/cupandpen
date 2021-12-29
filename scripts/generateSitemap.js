5e6fda033a465054dc4119dba1b9c27a1eaa5664
Richie Casto <rcasto92@gmail.com>
2021-12-29 14:10:52 -0500

    improve generate sitemap script some

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -1,32 +1,66 @@
 const fs = require('fs');
-const { SitemapStream, streamToPromise } = require('sitemap');
+const { SitemapStream, streamToPromise, parseSitemap } = require('sitemap');
 const formatXML = require('xml-formatter');
 const { getContentFiles } = require('./util');
 
 const siteUrl = 'https://cupandpen.com';
 const sitemapPath = 'docs/sitemap.xml';
 
-async function generateSitemap() {
+async function getExistingSitemapItems() {
+    const siteMapReadStream = fs.createReadStream(sitemapPath);
+    const sitemapItems = await parseSitemap(siteMapReadStream);
+    return sitemapItems;
+}
+
+async function getCurrentSitemapItems() {
+    const sitemapItems = [];
     const contentFiles = await getContentFiles();
-    const sitemap = new SitemapStream({
-        hostname: siteUrl
-    });
     const currentDate = new Date();
 
     // add content pages
     contentFiles
         .forEach(contentFile => {
-            sitemap.write({
-                url: `${siteUrl}/content/${contentFile.name}`,
+            sitemapItems.push({
+                url: `${siteUrl}/content/${encodeURIComponent(contentFile.name)}`,
                 lastmod: currentDate,
             });
         });
 
     // add index/home page
-    sitemap.write({
-        url: `${siteUrl}`,
+    sitemapItems.push({
+        url: `${siteUrl}/`,
         lastmod: currentDate,
     });
+
+    return sitemapItems;
+}
+
+async function generateSitemap() {
+    const sitemap = new SitemapStream({
+        hostname: siteUrl
+    });
+
+    const existingSitemapItems = await getExistingSitemapItems();
+    const currentSitemapItems = await getCurrentSitemapItems();
+
+    const existingSitemapItemMap = new Map();
+    existingSitemapItems
+        .forEach(existingSitemapItem =>
+            existingSitemapItemMap.set(existingSitemapItem.url, existingSitemapItem));
+
+    currentSitemapItems
+            .forEach(currentSitemapItem => {
+                // console.log(currentSitemapItem.url, existingSitemapItemMap.has(currentSitemapItem.url));
+
+                // sitemap item for current entry already existed in sitemap
+                // let's preserve the original sitemap record
+                if (existingSitemapItemMap.has(currentSitemapItem.url)) {
+                    const existingSitemapItemRecord = existingSitemapItemMap.get(currentSitemapItem.url);
+                    sitemap.write(existingSitemapItemRecord);
+                } else {
+                    sitemap.write(currentSitemapItem);
+                }
+            });
 
     sitemap.end();
 
